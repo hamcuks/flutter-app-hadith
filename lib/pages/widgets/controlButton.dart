d6bb7cbb5469a7d17b164f47a1ed890c409b25ad
hamcuks <ivannurilhan60@gmail.com>
2021-08-01 07:27:47 +0700

    refactor button

--- lib/pages/widgets/controlButton.dart
+++ lib/pages/widgets/controlButton.dart
@@ -5,25 +5,31 @@ class ControlButton extends StatelessWidget {
     Key? key,
     required this.icon,
     required this.onTap,
+    required this.page,
   }) : super(key: key);
 
   final IconData icon;
+  final int page;
   final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: 72,
-        height: 48,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            width: 1.5,
-            color: Color(0xFF1A645B),
-          ),
+    return Container(
+      width: 72,
+      height: 48,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          width: 1.5,
+          color: Color(0xFF1A645B),
         ),
+      ),
+      child: TextButton(
+        onPressed: page == 1
+            ? null
+            : page >= 1
+                ? onTap
+                : null,
         child: Icon(
           icon,
           color: Color(0xFF1A645B),
